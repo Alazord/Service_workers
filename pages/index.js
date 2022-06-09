12863e014cf4ef3888cec66f4ca3c4efae0bbc4b
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-10 04:34:58 +0530

    Components of of home page

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,56 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const MyImage = (props) => {
+    return (
+      <Image
+        src="/images/rickMorty.jpeg"
+        alt="Picture of rick n morty"
+        width={50}
+        height={50}
+      />
+    )
+  }
   return (
    <div className="all">
     <div className="home">Home</div>
     <div className="welcome">Welcome to Rick and Morty Wiki!</div>
-    <div className="carousal">Img to be shown</div>
+    <MyImage className="carousal" />
     <div className="decriptionBox">
       <div className="descriptionHead">Description</div>
       <div className="descriptionContent">abcd xyz</div>
     </div>
     <div className="mainCharactersBox">
       <div className="mainCharactersHead">Main Characters</div>
-      <div  className="mainCharactersContent">img list</div>
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+    </div>
+    <div className="episodeBox">
+        <div> Previous Episode </div>
+        <div> Next Episode</div>
+        <MyImage  />
+      <MyImage  />
+      <div> Name of Episode </div>
+        <div> Name of Episode</div>
+        <div> Description</div>
+        <div> Description</div>
+   </div>
+    <div className="Latest"> Latest Comic</div>
+    <div> <MyImage  /> 
+    <div> Name of comic</div>
+    <div> Description</div> 
+    </div>
+    <div className="exploreBox">
+      <div>Explore and Discover</div>
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
+      <MyImage  />
     </div>
    </div>
   )
