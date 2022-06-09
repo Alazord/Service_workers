8f8da72e3aeb37182849ccb498924cf16b6a5c5b
Alazord <Alazord@users.noreply.github.com>
2022-06-10 03:28:20 +0530

    Navbar created

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const optionList = ["Rick and Morty Wiki", "Explore", "Rick and Morty", "Episodes"]
+
   return (
-   <div>
-    Trying first PR
+   <div className= "nav-container" style={{display : 'flex', margin :'auto'}}>
+    {
+        optionList.map((item, index) =>(
+            <div className = "nav-element" key={index} style = {{ margin : '5px'}}>
+                {item}
+            </div>
+        ))
+    }
    </div>
   )
 }
