8695b8436353d95d71470207605473ea6d762cd4
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-10 03:26:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,18 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-   <div className="Home">
-    Trying to get unstuck
+   <div className="all">
+    <div className="home">Home</div>
+    <div className="welcome">Welcome to Rick and Morty Wiki!</div>
+    <div className="carousal">Img to be shown</div>
+    <div className="decriptionBox">
+      <div className="descriptionHead">Description</div>
+      <div className="descriptionContent">abcd xyz</div>
+    </div>
+    <div className="mainCharactersBox">
+      <div className="mainCharactersHead">Main Characters</div>
+      <div  className="mainCharactersContent">img list</div>
+    </div>
    </div>
   )
 }
