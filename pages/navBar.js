1cbb5e402b0285d0c7ea9ab61b8df566a044b592
Alazord <Alazord@users.noreply.github.com>
2022-06-13 09:10:25 +0530

    Home page updated with Styles

--- pages/navBar.js
+++ pages/navBar.js
@@ -1,15 +1,17 @@
-export default function NavBar (){
-    
-    const optionList = ["Rick and Morty Wiki", "Explore", "Rick and Morty", "Episodes"]
-    return (
-        <div className= "nav-container" style={{display : 'flex', margin :'auto'}}>
-    {
-        optionList.map((item, index) =>(
-            <div className = "nav-element" key={index} style = {{ margin : '5px'}}>
-                {item}
-            </div>
-        ))
-    }
+export default function NavBar() {
+  const optionList = [
+    "RICK AND MORTY WIKI",
+    "EXPLORE",
+    "RICK AND MORTY",
+    "EPISODES",
+  ];
+  return (
+    <div className="nav-container" style={{ display: "flex" }}>
+      {optionList.map((item, index) => (
+        <div className="nav-element" key={index}>
+          {item}
+        </div>
+      ))}
     </div>
-    )
+  );
 }
