4779e8b7733862d284b6863c57a5dd97117f65da
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-18 02:51:56 +0530

    Episode page styled

--- pages/character/[char].js
+++ pages/character/[char].js
@@ -1,21 +1,7 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
-import { useState } from "react";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-import styles from "../../styles/Home2.module.css";
-import {
-  Input,
-  Stack,
-  IconButton,
-  Box,
-  Flex,
-  useToast,
-} from "@chakra-ui/react";
-import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
 import Image from "next/image";
 import Link from "next/link";
-import { Heading, Text, SimpleGrid } from "@chakra-ui/react";
-import Char from "../../components/Char";
 
 export default function MyChar(results) {
   const character = results.character;
@@ -25,18 +11,6 @@ export default function MyChar(results) {
         <title>Character Details</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {/* <Box mb={4} flexDirection="column" align="center" justify="center" py={8}>
-      <Heading as="h1" size="2xl" mb={8}>
-        Details of the character you clicked on:{" "}
-      </Heading>
-      <Image alt="" src={character.image} width={300} height={300} />
-      <Text align="center">Species: {character.species}</Text>
-      <Text align="center">Status: {character.status}</Text>
-      <Text align="center">Name: {character.name}</Text>
-      <Text align="center">Gender: {character.gender}</Text>
-      <Text align="center">Created: {character.created}</Text>
-      </Box> */}
       <h1
         style={{
           backgroundColor: "purple",
@@ -84,7 +58,7 @@ export default function MyChar(results) {
           borderColor: "white",
         }}
       >
-        <a href="/char_page">Return</a>
+        <Link href={"/char_page"}>Return</Link>
       </button>
     </div>
   );
@@ -92,8 +66,6 @@ export default function MyChar(results) {
 
 export async function getServerSideProps(context) {
   const id = context.params.char;
-  // const { query } = context
-  // console.log(query);
   const client = new ApolloClient({
     uri: "https://rickandmortyapi.com/graphql/",
     cache: new InMemoryCache(),
--- pages/episode/[epi].js
+++ pages/episode/[epi].js
@@ -1,63 +1,106 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
-import { useState } from "react";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-import styles from "../../styles/Home2.module.css";
-import {
-  Input,
-  Stack,
-  IconButton,
-  Box,
-  Flex,
-  useToast,
-} from "@chakra-ui/react";
-import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
-import Image from "next/image";
 import Link from "next/link";
-import { Heading, Text, SimpleGrid } from "@chakra-ui/react";
-import Char from "../../components/Char";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-export default function MyChar(results) {
+// export default function MyChar(results) {
+//   const episode = results.episode;
+//   return (
+//     <Flex direction="column" justify="center" align="center">
+//       <Head>
+//         <title>Episode Details</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <Box
+//         mb={4}
+//         flexDirection="column"
+//         align="center"
+//         justify="center"
+//         py={8}
+//         backgroundColor="white"
+//         marginTop={80}
+//         height={350}
+//         padding={10}
+//         borderRadius={20}
+//         opacity={0.9}
+//       >
+//         <Heading as="h1" size="2xl" mb={8}>
+//           Details of the episode you clicked on:{" "}
+//         </Heading>
+//         <Text align="center">Episode: {episode.episode}</Text>
+//         <Text align="center">Air-Date: {episode.air_date}</Text>
+//         <Text align="center">Name: {episode.name}</Text>
+//         <Text align="center">Created: {episode.created}</Text>
+//       </Box>
+//       <button style={({ width: "100px" }, { fontSize: "24px" })}>
+//         <a href="/episode_page">Return</a>
+//       </button>
+//     </Flex>
+//   );
+// }
+
+export default function MyEpisode(results) {
   const episode = results.episode;
   return (
-    <Flex direction="column" justify="center" align="center">
+    <div className="episodeCard">
       <Head>
         <title>Episode Details</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Box
-        mb={4}
-        flexDirection="column"
-        align="center"
-        justify="center"
-        py={8}
-        backgroundColor="white"
-        marginTop={80}
-        height={350}
-        padding={10}
-        borderRadius={20}
-        opacity={0.9}
+      <h1
+        style={{
+          backgroundColor: "purple",
+          color: "white",
+          padding: "10px",
+          borderRadius: "5px",
+        }}
       >
-        <Heading as="h1" size="2xl" mb={8}>
-          Details of the episode you clicked on:{" "}
-        </Heading>
-        <Text align="center">Episode: {episode.episode}</Text>
-        <Text align="center">Air-Date: {episode.air_date}</Text>
-        <Text align="center">Name: {episode.name}</Text>
-        <Text align="center">Created: {episode.created}</Text>
-      </Box>
-      <button style={({ width: "100px" }, { fontSize: "24px" })}>
-        <a href="/episode_page">Return</a>
+        Details of the Episode you clicked on:
+      </h1>
+      <div
+        style={{
+          alignItems: "center",
+          padding: "10px",
+          borderRadius: "10px",
+          backgroundColor: "rgba(255,255,255,0.7)",
+        }}
+      >
+        <ul
+          style={{ listStyle: "none", alignItems: "center", fontSize: "21px" }}
+        >
+          <li>
+            <b>Episode:</b> {episode.episode}
+          </li>
+          <li>
+            <b>Air-Date:</b> {episode.air_date}
+          </li>
+          <li>
+            <b>Name:</b> {episode.name}
+          </li>
+          <li>
+            <b>Created:</b> {episode.created}
+          </li>
+        </ul>
+      </div>
+      <button
+        style={{
+          padding: "10px",
+          fontSize: "18px",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          borderRadius: "10px",
+          width: "150px",
+          marginTop: "10px",
+          borderColor: "white",
+        }}
+      >
+        <Link href={"/episode_page"}>Return</Link>
       </button>
-    </Flex>
+    </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const id = context.params.epi;
-  // const { query } = context
-  // console.log(query);
   const client = new ApolloClient({
     uri: "https://rickandmortyapi.com/graphql/",
     cache: new InMemoryCache(),
