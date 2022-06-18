5d43e9eb06b69bf2f1f7627240cd1231b21438ff
Alazord <Alazord@users.noreply.github.com>
2022-06-18 18:24:38 +0530

    Minor design changes

--- components/Episode.js
+++ components/Episode.js
@@ -3,21 +3,23 @@ import Link from "next/link";
 
 const Episode = ({ episodes }) => {
   return (
-    <div className="charInd"> 
+    <div className="charInd">
       {episodes.map((episode) => {
         return (
           <div key={episode.id}>
             <Link href={`/episode/${episode.id}`}>
               <div className="charItem">
-                <h2 className="charTxt">{episode.name}</h2>
+                <h2 className="charTxt" id="heading">
+                  {episode.name}
+                </h2>
                 <h4 className="charTxt">Episode: {episode.episode}</h4>
                 <h4 className="charTxt">Name: {episode.name}</h4>
               </div>
             </Link>
-            </div>
+          </div>
         );
       })}
-       </div>
+    </div>
   );
 };
 
