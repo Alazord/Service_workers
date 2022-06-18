fce778e18f8d8bed89973e0078c8612bacc6b9a8
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-18 10:16:31 +0530

    fallback

--- pages/fallback.jsx
+++ pages/fallback.jsx
@@ -1,7 +1,8 @@
+import { color } from '@chakra-ui/react';
 import React from 'react';
 
 const fallback = () => (
-  <div>
+  <div className='fallback'>
     <h1>This is fallback page when device is offline </h1>
     <h3>Route will fallback to this page</h3>
   </div>
