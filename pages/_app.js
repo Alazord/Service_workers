66907bd89a2469c5a8135c6caf71c82171cd53d0
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-27 14:30:12 +0530

    experiments

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,14 @@
 import "../styles/globals.css";
 import { useEffect } from "react";
-// import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { AsyncStorageWrapper, CachePersistor } from "apollo3-cache-persist";
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/",
   cache: new InMemoryCache(),
-  // method: "POST",
-  // mode: "cors",
-  // headers: {
-  // "Content-Type": "application/json",
-  // // 'Cache-Control': 'max-age=60',
-  // },
 });
 
 function MyApp({ Component, pageProps }) {
-//   const persistor = new CachePersistor({
-//     cache,
-//     storage: new AsyncStorageWrapper(AsyncStorage),
-//  });
-//  // then later when initializing your App
-//  useEffect(() => {
-//     async function initializeCache() {
-//        await persistor.restore();
-//        const client = new ApolloClient({
-//           // your Apollo Client initialization
-//           uri: "https://rickandmortyapi.com/graphql/",
-//           cache: new InMemoryCache(),
-//        });
-//        client.onClearStore(async () => {
-//           await persistor.purge();
-//        });
-//     }
-//     initializeCache();
-//  }, []);  
-
+  
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
