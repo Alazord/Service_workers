d0d8e561b900aa8aa56c3592d3285a49bca567a3
nik5693 <nikunj.madhogaria@sprinklr.com>
2022-06-27 12:34:10 +0530

    Update episodePage.js

--- pages/episodePage.js
+++ pages/episodePage.js
@@ -173,41 +173,6 @@ import fetchEpisodeList from "../components/fetchEpisodeList";
 
 const queryClient = new QueryClient();
 
-// const fetchEpisodeList = async ({queryKey}) => {
-//   try{
-//     const [_key, search] = queryKey
-//     const res = await fetch("https://rickandmortyapi.com/graphql/", {
-//         method: "POST",
-//         mode: "cors",
-//         headers: {
-//         "Content-Type": "application/json",
-//         'Cache-Control': 'max-age=3600',
-//         },
-//         body: JSON.stringify({
-//         query: `
-//             query getEpisodes{
-//             episodes(filter: { name: "${search}" }) {
-//                 results {
-//                 name
-//                 id
-//                 air_date
-//                 episode
-//                 created
-//                 }
-//             } 
-//             }
-//         `,
-//         }),
-//     });
-//     const episode_object= await res.json();
-//     const episode_list=episode_object.data.episodes.results;
-//     return episode_list;
-//   }
-//   catch(error){
-//     Router.push("/searchFallback");
-//   }
-// };
-
 const Episode_List = () => {
     const [search, setSearch] = useState("");
     const query_id="Episodes";
